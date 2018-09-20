3c228d19816cc502952c2a5a7b59c2c2dd4b339a
Tom <thomasyoung@Thomass-MacBook.local>
2018-09-20 11:37:52 -0400

    generator and training

--- GAN.py
+++ GAN.py
@@ -1,10 +1,10 @@
-# DCGAN digit recognition and generation
 # Tom Young - Fall 2018
 # Adapted from Felix Mohr's tutorial
 # https://github.com/FelixMohr/Deep-learning-with-Python/blob/master/DCGAN-MNIST.ipynb
-
+%matplotlib inline
 import tensorflow as tf
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -61,3 +61,128 @@ def discriminator(img_in, reuse=None, keep_prob=keep_prob):
         return x
 
 
+def generator(z, keep_prob=keep_prob, is_training=is_training):
+    activation = lrelu
+    momentum = 0.99
+    with tf.variable_scope("generator", reuse=None):
+        x = z
+        d1 = 4
+        d2 = 1
+
+        # reverse process as discriminator
+        x = tf.layers.dense(x, units=d1 * d1 * d2, activation=activation)
+        x = tf.layers.dropout(x, keep_prob)
+
+        # batch normalization
+        x = tf.contrib.layers.batch_norm(x, is_training=is_training, decay=momentum)
+        x = tf.reshape(x, shape=[-1, d1, d1, d2])
+
+        # convolutions
+        x = tf.image.resize_images(x, size=[7, 7])
+        x = tf.layers.conv2d_transpose(x, kernel_size=5, filters=64, strides=2, padding='same', activation=activation)
+        x = tf.layers.dropout(x, keep_prob)
+        x = tf.contrib.layers.batch_norm(x, is_training=is_training, decay=momentum)
+        x = tf.layers.conv2d_transpose(x, kernel_size=5, filters=64, strides=2, padding='same', activation=activation)
+        x = tf.layers.dropout(x, keep_prob)
+        x = tf.contrib.layers.batch_norm(x, is_training=is_training, decay=momentum)
+        x = tf.layers.conv2d_transpose(x, kernel_size=5, filters=64, strides=1, padding='same', activation=activation)
+        x = tf.layers.dropout(x, keep_prob)
+        x = tf.contrib.layers.batch_norm(x, is_training=is_training, decay=momentum)
+        x = tf.layers.conv2d_transpose(x, kernel_size=5, filters=1, strides=1, padding='same', activation=tf.nn.sigmoid)
+        return x
+
+
+g = generator(noise, keep_prob, is_training)
+
+# for training discriminator
+d_real = discriminator(X_in)
+
+# for training generator
+d_fake = discriminator(g, reuse=True)
+
+# data separation between desc. and gen.
+vars_g = [var for var in tf.trainable_variables() if var.name.startswith("generator")]
+vars_d = [var for var in tf.trainable_variables() if var.name.startswith("discriminator")]
+
+# regularization
+d_reg = tf.contrib.layers.apply_regularization(tf.contrib.layers.l2_regularizer(1e-6), vars_d)
+g_reg = tf.contrib.layers.apply_regularization(tf.contrib.layers.l2_regularizer(1e-6), vars_g)
+
+# loss function for training and testing
+loss_d_real = binary_cross_entropy(tf.ones_like(d_real), d_real)
+loss_d_fake = binary_cross_entropy(tf.zeros_like(d_fake), d_fake)
+
+loss_g = tf.reduce_mean(binary_cross_entropy(tf.ones_like(d_fake), d_fake))
+loss_d = tf.reduce_mean(0.5 * (loss_d_real + loss_d_fake))
+
+update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+with tf.control_dependencies(update_ops):
+    optimizer_d = tf.train.RMSPropOptimizer(learning_rate=0.00015).minimize(loss_d + d_reg, var_list=vars_d)
+    optimizer_g = tf.train.RMSPropOptimizer(learning_rate=0.00015).minimize(loss_g + g_reg, var_list=vars_g)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+# Code by Parag Mital (github.com/pkmital/CADL)
+def montage(images):
+    if isinstance(images, list):
+        images = np.array(images)
+    img_h = images.shape[1]
+    img_w = images.shape[2]
+    n_plots = int(np.ceil(np.sqrt(images.shape[0])))
+    m = np.ones((images.shape[1] * n_plots + n_plots + 1, images.shape[2] * n_plots + n_plots + 1)) * 0.5
+    for i in range(n_plots):
+        for j in range(n_plots):
+            this_filter = i * n_plots + j
+            if this_filter < images.shape[0]:
+                this_img = images[this_filter]
+                m[1 + i + i * img_h:1 + i + (i + 1) * img_h,
+                  1 + j + j * img_w:1 + j + (j + 1) * img_w] = this_img
+    return m
+
+# Training (60k iters)
+for i in range(60000):
+    print(i)
+    train_d = True
+    train_g = True
+    keep_prob_train = 0.6
+
+    # random noise
+    n = np.random.uniform(0.0, 1.0, [batch_size, n_noise]).astype(np.float32)
+    batch = [np.reshape(b, [28, 28]) for b in mnist.train.next_batch(batch_size=batch_size)[0]]
+
+    d_real_ls, d_fake_ls, g_ls, d_ls = sess.run([loss_d_real, loss_d_fake, loss_g, loss_d],
+                                                feed_dict={X_in: batch, noise: n, keep_prob: keep_prob_train,
+                                                           is_training: True})
+
+    d_real_ls = np.mean(d_real_ls)
+    d_fake_ls = np.mean(d_fake_ls)
+    g_ls = g_ls
+    d_ls = d_ls
+
+    if g_ls * 1.5 < d_ls:
+        train_g = False
+        pass
+    if d_ls * 2 < g_ls:
+        train_d = False
+        pass
+
+    if train_d:
+        sess.run(optimizer_d, feed_dict={noise: n, X_in: batch, keep_prob: keep_prob_train, is_training: True})
+
+    if train_g:
+        sess.run(optimizer_g, feed_dict={noise: n, keep_prob: keep_prob_train, is_training: True})
+
+    if not i % 50:
+        print(i, d_ls, g_ls, d_real_ls, d_fake_ls)
+        if not train_g:
+            print("not training generator")
+        if not train_d:
+            print("not training discriminator")
+        gen_img = sess.run(g, feed_dict={noise: n, keep_prob: 1.0, is_training: False})
+        imgs = [img[:, :, 0] for img in gen_img]
+        m = montage(imgs)
+        gen_img = m
+        plt.axis('off')
+        plt.imshow(gen_img, cmap='gray')
+        plt.show()
